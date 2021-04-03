b3fd35419f754a71295604d46ff2c95cc4235e2f
Chenglong Wang <clwang@phy.ecnu.edu.cn>
2021-04-03 13:48:44 +0800

    Add FullMask2DSliceCropD.

--- monai_ex/transforms/croppad/dictionary.py
+++ monai_ex/transforms/croppad/dictionary.py
@@ -1,11 +1,11 @@
-from typing import Dict, Hashable, Mapping, Optional, Sequence, Union
+from typing import Dict, Hashable, Mapping, Optional, Sequence, Union, List
 
 import numpy as np
 
 from monai.config import KeysCollection
 from monai.transforms.compose import MapTransform
 
-from monai_ex.transforms.croppad.array import CenterMask2DSliceCrop
+from monai_ex.transforms.croppad.array import CenterMask2DSliceCrop, FullMask2DSliceCrop
 
 
 class CenterMask2DSliceCropd(MapTransform):
@@ -17,6 +17,7 @@ class CenterMask2DSliceCropd(MapTransform):
         crop_mode: str,
         center_mode: str,
         z_axis: int,
+        n_slices: int = 3
     ) -> None:
         super().__init__(keys)
         self.mask_key = mask_key
@@ -25,7 +26,8 @@ class CenterMask2DSliceCropd(MapTransform):
             crop_mode=crop_mode,
             center_mode=center_mode,
             z_axis=z_axis,
-            mask_data=None
+            mask_data=None,
+            n_slices=n_slices
         )
     
     def __call__(self, data: Mapping[Hashable, np.ndarray]) -> Dict[Hashable, np.ndarray]:
@@ -35,4 +37,42 @@ class CenterMask2DSliceCropd(MapTransform):
         return d
 
 
+class FullMask2DSliceCropd(MapTransform):
+    def __init__(
+        self,
+        keys: KeysCollection,
+        mask_key: KeysCollection,
+        roi_size: Union[Sequence[int], int],
+        crop_mode: str,
+        z_axis: int,
+        n_slices: int = 3
+    ) -> None:
+        super().__init__(keys)
+        self.mask_key = mask_key
+        self.cropper = FullMask2DSliceCrop(
+            roi_size=roi_size,
+            crop_mode=crop_mode,
+            z_axis=z_axis,
+            mask_data=None,
+            n_slices=n_slices
+        )
+    
+    def __call__(self, data: Mapping[Hashable, np.ndarray]) -> Dict[Hashable, np.ndarray]:
+        d = dict(data)
+        mask = d[self.mask_key]
+        centers = self.cropper.get_center_pos_(mask)
+        results: List[Dict[Hashable, np.ndarray]] = [dict() for _ in centers]
+        for key in data.keys():
+            if key in self.keys:
+                img = d[key]
+                for i, crop in enumerate(self.cropper(img, msk=mask)):
+                    results[i][key] = crop
+            else:
+                for i in range(len(centers)):
+                    results[i][key] = data[key]
+        
+        return results
+                
+
 CenterMask2DSliceCropD = CenterMask2DSliceCropDict = CenterMask2DSliceCropd
+FullMask2DSliceCropD = FullMask2DSliceCropDict = FullMask2DSliceCropd
