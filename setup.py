b8e50a58fe6b460cb6a6a15fb95567f3e6090792
Chenglong Wang <clwang@phy.ecnu.edu.cn>
2021-05-26 10:58:41 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_namespace_packages
 
 setup(name='monai_ex',
       packages=find_namespace_packages(include=["monai_ex", "monai_ex.*"]),
-      version='0.0.2',
+      version='0.0.3',
       description='MONAI extension',
       author='Chenglong Wang',
       author_email='clwang@phy.ecnu.edu.cn',
       license='Apache License Version 2.0, January 2004',
       install_requires=[
             "h5py",
-            "torch>=1.5.0",
-            "monai==0.4.0",
+            "torch>=1.6.0",
+            "monai==0.5.0",
       ],
       keywords=['deep learning', 'monai', 'pytorch']
       )
