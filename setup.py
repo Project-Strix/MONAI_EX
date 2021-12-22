9f3bc1c7ba8a630730bcc7960b2e45d69aae2e06
Chenglong Wang <clwang@phy.ecnu.edu.cn>
2021-12-22 16:07:12 +0800

    Add matplotlb install require.

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(name='monai_ex',
       license='Apache License Version 2.0, January 2004',
       install_requires=[
             "h5py",
+            "matplotlib==3.3.3",
             "torch>=1.6.0",
             "monai==0.8.0",
       ],
