1292a6b130a6f1e4292c93103efeff3ee2fdfedf
Chenglong <ryuu.j.ching@gmail.com>
2022-05-15 20:27:22 +0800

    Use versioneer. Added setup.cfg

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 from setuptools import setup, find_namespace_packages
+import versioneer
 
 setup(name='monai_ex',
       packages=find_namespace_packages(include=["monai_ex", "monai_ex.*"]),
-      version='0.0.5',
+      version=versioneer.get_version(),
       description='MONAI extension',
       author='Chenglong Wang',
       author_email='clwang@phy.ecnu.edu.cn',
       license='Apache License Version 2.0, January 2004',
-      install_requires=[
-            "h5py",
-            "matplotlib==3.3.3",
-            "torch>=1.6.0",
-            "monai==0.8.0",
-      ],
+      cmdclass=versioneer.get_cmdclass(),
       keywords=['deep learning', 'monai', 'pytorch']
       )
